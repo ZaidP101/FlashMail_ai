c844600c1971cdf96b06c2f562b407174c0341dc
ZaidP101 <zpatel044@gmail.com>
2026-07-16 00:23:24 +0530

    feat: add PostgreSQL, JPA dependencies and datasource config

--- src/main/java/com/zaid/patel/AI_Email_Assistant/AiEmailAssistantApplication.java
+++ src/main/java/com/zaid/patel/AI_Email_Assistant/AiEmailAssistantApplication.java
@@ -2,9 +2,8 @@ package com.zaid.patel.AI_Email_Assistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class AiEmailAssistantApplication {
 
 	public static void main(String[] args) {
